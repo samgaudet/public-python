99ab3961b61d83bfeae0638b2e47e9245f8651f7
Sam Gaudet <sgaudet1@babson.edu>
2020-04-17 14:11:43 -0400

    add practice / example file

---
+++ gsuite/sample.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from gsuite.spreadsheet import Spreadsheet
+from gsuite.service_connection import connect_service
+
+SPREADSHEET_ID = "1Q3n6HDCsCFpChaiHA2s_wFRoIuJ0_hsOurPvfq4eRXI"
+RANGE = "test_sheet_1!A1:B11"
+
+
+def get_data_from_gsheet(service, spreadsheetId, range):
+
+    sheet_data = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheetId, range=range).execute()
+
+    return sheet_data
+
+
+service = connect_service()
+test_data = get_data_from_gsheet(
+    service=service, spreadsheetId=SPREADSHEET_ID, range=RANGE)
+
+test_spreadsheet = Spreadsheet(client=service, spreadsheetId=SPREADSHEET_ID)
+print(test_spreadsheet.sheets)
