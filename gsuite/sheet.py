7839700fa56ddbc433458d7b07c1f6497d304762
Sam Gaudet <sgaudet1@babson.edu>
2020-04-18 19:33:50 -0400

    add properties

--- gsuite/sheet.py
+++ gsuite/sheet.py
@@ -6,8 +6,29 @@ class Sheet:
     def __init__(self, sheet, spreadsheet,):
 
         self._sheet = sheet
+        self._properties = sheet["properties"]
         self._spreadsheet = spreadsheet
 
     @property
     def sheetId(self):
-        return self._sheet["sheetId"]
+        return self._properties["sheetId"]
+
+    @property
+    def title(self):
+        return self._properties["title"]
+
+    @property
+    def sheetIndex(self):
+        return self._properties["index"]
+
+    @property
+    def sheetType(self):
+        return self._properties["sheetType"]
+
+    @property
+    def rowCount(self):
+        return self._properties["rowCount"]
+
+    @property
+    def columnCount(self):
+        return self._properties["columnCount"]
